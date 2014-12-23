b5ca261a5813bdc190d1b70cfc5e19ce8066c0e9
chrisdevwords <chrisedwards82@ChrisEdwardss-MacBook-Pro.local>
2014-12-23 09:54:29 -0500

    working REST endpoint for clip by id

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,6 +5,79 @@ var router  = express.Router();
 var pg      = require('pg');
 var YouTube = require('youtube-node');
 
+var TABLE = 'test_table';
+
+var getClips = function (req, res) {
+    
+    var sql = "SELECT * FROM " + TABLE;
+    var id = req.params.id;
+    var resultData;
+    if (id && id.length) {
+        sql += ' WHERE uid = ' + id;
+    }
+    
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(sql, function (err, result) {
+            done();
+            if (err) {
+                res.render('error', {
+                    message: err.message || 'Database Error',
+                    error: err
+                });
+            } else {
+                resultData = result.rows.length > 1 ? result.rows : result.rows[0];
+                res.send(resultData);
+            }
+        });
+    });
+};
+
+var saveClip = function (req, res) {
+    
+    var ytid = req.body.ytid;
+    var int_start = Math.floor(req.body.int_start);
+    var int_end = Math.floor(req.body.int_end);
+    var errorMsg;
+    var sql;
+    if (!ytid || !ytid.length) {
+        errorMsg = 'Malformed or missing ytid';
+    }
+    if (isNaN(int_start) || isNaN(int_start)) {
+        errorMsg = 'Malformed or missing start and endpoints.';
+    }
+    if (errorMsg) {
+        res.send({
+            error : {message : errorMsg}
+        });
+        return;
+    }
+    sql = "INSERT INTO " + TABLE + " values('" +
+        ytid + "', " + 
+        int_start + ", " +
+        int_end +
+    ") RETURNING *;";
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(sql, function (err, result) {
+            done();
+            if (err) {
+                res.send({
+                    error:{
+                        message: err.message || 'Database Error',
+                        error: err,
+                        sql: sql
+                    }
+                });
+            } else {
+                res.send(result.rows);
+            }
+        });
+    });
+};
+
+router.get('/clip', getClips);
+router.get('/clip/:id', getClips);
+
+router.post('/clip', saveClip);
 
 router.get('/youtube', function(req, res) {
     var q = req.param('q') || '';
@@ -14,25 +87,6 @@ router.get('/youtube', function(req, res) {
     youTube.setKey(process.env.YOUTUBE_KEY || req.param('youtube_key') || '');
     youTube.search(q, count, function(resultData) {
         res.send(resultData);
-    });
-});
-
-router.get('/db', function(req, res) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function (err, result) {
-            done();
-            if (err) {
-                res.render('error', {
-                    message: err.message || 'Database Error',
-                    error: err
-                });
-            } else {
-                res.render('index', {
-                    title: 'Data!',
-                    data: JSON.stringify(result.rows)
-                })
-            }
-        });
     });
 });
 
